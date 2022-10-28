dffe3059784da0c1d1fdd383903a3cd7c0960727
eelizabethriley <eeriley@smith.edu>
2022-10-28 17:04:25 -0400

    Implemented constructor & methods

--- Cafe.java
+++ Cafe.java
@@ -1,13 +1,37 @@
 /* This is a stub for the Cafe class */
 public class Cafe extends Building {
+    private int nCoffeeOunces; // The number of ounces of coffee remaining in inventory
+    private int nSugarPackets; // The number of sugar packets remaining in inventory
+    private int nCreams; // The number of "splashes" of cream remaining in inventory
+    private int nCups; // The number of cups remaining in inventory
 
-    public Cafe(String name, String address, int nFloors) {
+    public Cafe(String name, String address, int nFloors, int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
         super(name, address, nFloors);
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
         System.out.println("You have built a cafe: ☕");
+    }
+
+    public void sellCoffee(int size, int nSugarPackets, int nCreams) {
+        if (nCoffeeOunces > this.nCoffeeOunces || nSugarPackets > this.nSugarPackets || nCreams > this.nCreams || this.nCups == 0) {
+            this.restock(nCoffeeOunces, nSugarPackets, nCreams);
+        }
+        this.nCoffeeOunces -= size;
+        this.nSugarPackets -= nSugarPackets;
+        this.nCreams --;
+        System.out.println(size + " oz coffee with " + nSugarPackets + " sugar(s) and " + nCreams + " cream(s) has been sold.");
+    }
+    
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams) {
+        this.nCoffeeOunces += nCoffeeOunces;
+        this.nSugarPackets += nSugarPackets;
+        this.nCreams += nCreams;
     }
     
     public static void main(String[] args) {
-        new Cafe("Campus Center Cafe", "100 Elm St, Northampton, MA 01063", 3);
+        new Cafe("Campus Center Cafe", "100 Elm St, Northampton, MA 01063", 3, 145, 30, 25, 20);
     }
     
 }
