34804c49dc5ecf6c174fe9778ae627c85386322c
eelizabethriley <eeriley@smith.edu>
2022-10-28 11:30:20 -0400

    Implemented constructor for library class + etc

--- Library.java
+++ Library.java
@@ -1,12 +1,45 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
+/* This is a stub for the Library class */
+public class Library extends Building {
+
+    private Hashtable<String, Boolean> collection;
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean>();
       System.out.println("You have built a library: 📖");
     }
   
+    public void addTitle(String title) {
+      collection.put(title, true);
+      System.out.println(title + " has ben added to " + this.getName() + "'s collection.");
+    }
+
+    public void removeTitle(String title) {
+      collection.remove(title);
+      System.out.println(title + " has been removed from " + this.getName() + "'s collection.");
+    }
+
+    public void checkOut(String title) {
+      collection.replace(title, true, false);
+      System.out.println("Successfully checked out the title " + title + " from " + this.getName() + ". Thank you!");
+    }
+
+    public void returnBook(String title) {
+      collection.replace(title, false, true);
+      System.out.println("Succesfully returned the title " + title + " to " + this.getName() + ". Thank you!");
+    }
+
     public static void main(String[] args) {
-      new Library();
+      Library neilson = new Library("Neilson", "7 Neilson Drive, Northampton MA 01063", 4);
+      neilson.addTitle("Pride and Prejudice by Jane Austen");
+      neilson.addTitle("The Bell Jar by Sylvia Plath");
+      neilson.checkOut("Pride and Prejudice by Jane Austen");
+      neilson.removeTitle("The Bell Jar by Sylvia Plath");
+      neilson.addTitle("The House on Mango Street by Sandra Cisneros");
+      neilson.checkOut("The House on Mango Street by Sandra Cisneros");
+      neilson.returnBook("Pride and Prejudice by Jane Austen");
     }
   
   }
